28f00e8416e6de8e35da9cd44fd98dc81032793f
harshtyagikhd <harshtyagikhd@gmail.com>
2026-03-13 14:26:49 +0530

    exp

---
+++ javascript unit-2/script.js
@@ -0,0 +1,37 @@
+ console.log("Hello world");
+var a=20;
+console.log(a);
+var a=30;
+console.log(a);
+{const name="Harshit";
+console.log(name);}
+let Name="sahu";
+console.log(typeof Name);
+let stu={
+    name:"Harshit",
+    age:19,
+    email:"harshitgts777@gmail.com"
+}
+console.log(stu.email);
+Arithmatic Operators
+let a=10;
+let b=5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a++);
+console.log(++a);
+console.log(--a);
+console.log(a--);
+console.log(a);
+Assignment Operators
+let a=10;
+a+=5;
+console.log(a);
+a-=3;
+console.log(a);
+a*=2;
+console.log(a);
+a/=4;
+console.log(a);
